afff1a6336fdf5435f4b0a3ead0817e63916568f
warcilac9 <114256884+warcilac9@users.noreply.github.com>
2023-09-06 22:33:04 -0600

    Inicie el script y el nav mesh para que el niño pueda patrullar

---
+++ Assets/Scripts/EnemyPatrolling}.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPatrolling : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Transform[] waypoints;
+    int waypointIndex;
+    Vector3 target; 
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        UpdateDestination();    
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, target) < 1)
+        {
+            IterateWaypointindex(); 
+            UpdateDestination();
+        }
+    }
+
+    void UpdateDestination()
+    {
+        target = waypoints[waypointIndex].position;
+        agent.SetDestination(target);
+    }
+    void IterateWaypointindex()
+    {
+        waypointIndex++;
+        if (waypointIndex == waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+    }
+
+    //minuto 4:03 https://youtu.be/c8Nq19gkNfs?si=u00H5e9s9iG-dIp8
+}
